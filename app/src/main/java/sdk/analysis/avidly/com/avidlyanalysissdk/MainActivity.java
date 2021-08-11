a3eb4685e3e8946d2b28d543e2f8ee5087811f03
jisheyu <m18795788399@163.com>
2021-08-11 18:19:48 +0800

    tasdk4105

--- app/src/main/java/sdk/analysis/avidly/com/avidlyanalysissdk/MainActivity.java
+++ app/src/main/java/sdk/analysis/avidly/com/avidlyanalysissdk/MainActivity.java
@@ -1,10 +1,11 @@
 package sdk.analysis.avidly.com.avidlyanalysissdk;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aly.account.ALYLogin;
@@ -35,61 +36,80 @@ public class MainActivity extends AppCompatActivity {
 
     //private AvidlyPlayableInterstitialAd playILAd;
     private static final String AF_DEV_KEY = "fZvuk792H9hJQKmaTwuXxA";
-    public static final String TAG = "aly_demoZ";
+    public static final String TAG = "tasdk_demo";
     private BillingClient billingClient;
     private String skuId = "Your goods id in google play console";
-    private String productId="999999";
+    private String productId="600215";
+    private TextView txtCont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtCont = findViewById(R.id.txtContent);
         // TODO:上线前请记得设置为false
         ALYAnalysis.enalbeDebugMode(true);
-        ALYAnalysis.init(getApplicationContext(), productId, "32408");
-        DurationReport.initReport("uid001");
-
-        initGoogleBilling();
-        AppsFlyerConversionListener conversionListener = new AppsFlyerConversionListener() {
+        ALYAnalysis.init(getApplicationContext(), productId, "32408", new ALYAnalysis.TasdkinitializdListener() {
             @Override
-            public void onConversionDataSuccess(Map<String, Object> conversionData) {
-                for (String attrName : conversionData.keySet()) {
-                    Log.d(TAG, "attribute: " + attrName + " = " + conversionData.get(attrName));
-                }
-                conversionData.put("Campaign", "600107_DW1_GP_L7_0410_WW_VO_Casino/Gambling");
-                ALYAnalysis.getConversionData(conversionData, new AFConversionDataResultListener() {
+            public void onSuccess(String s) {
+                txtCont.setText("init onSuccess userId is: "+s);
+                ALYAnalysis.setAFId(AppsFlyerLib.getInstance().getAppsFlyerUID(getApplicationContext()));
+                DurationReport.initReport("uid001");
+
+                initGoogleBilling();
+
+                AppsFlyerConversionListener conversionListener = new AppsFlyerConversionListener() {
                     @Override
-                    public void onSuccess(String s) {
-                        Log.i(TAG, "onSuccess: " + s);
+                    public void onConversionDataSuccess(Map<String, Object> conversionData) {
+                        for (String attrName : conversionData.keySet()) {
+                            Log.d(TAG, "attribute: " + attrName + " = " + conversionData.get(attrName));
+                        }
+                        conversionData.put("Campaign", "600107_DW1_GP_L7_0410_WW_VO_Casino/Gambling");
+                        ALYAnalysis.getConversionData(conversionData, new AFConversionDataResultListener() {
+                            @Override
+                            public void onSuccess(String s) {
+                                Log.i(TAG, "onSuccess: " + s);
+
+                            }
+
+                            @Override
+                            public void onFail(String s) {
+                                Log.i(TAG, "onFail: " + s);
+                            }
+                        });
                     }
 
                     @Override
-                    public void onFail(String s) {
-                        Log.i(TAG, "onFail: " + s);
+                    public void onConversionDataFail(String errorMessage) {
+                        Log.d(TAG, "error getting conversion data: " + errorMessage);
                     }
-                });
+
+                    @Override
+                    public void onAppOpenAttribution(Map<String, String> conversionData) {
+
+                        for (String attrName : conversionData.keySet()) {
+                            Log.d(TAG, "attribute: " + attrName + " = " + conversionData.get(attrName));
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onAttributionFailure(String errorMessage) {
+                        Log.d(TAG, "error onAttributionFailure : " + errorMessage);
+                    }
+                };
+                AppsFlyerLib.getInstance().init(AF_DEV_KEY, conversionListener, getApplicationContext());
+                AppsFlyerLib.getInstance().startTracking(getApplication());
+
+
             }
 
             @Override
-            public void onConversionDataFail(String errorMessage) {
-                Log.d(TAG, "error getting conversion data: " + errorMessage);
+            public void onFail(String s) {
+                txtCont.setText("init fail "+ s);
             }
+        });
 
-            @Override
-            public void onAppOpenAttribution(Map<String, String> conversionData) {
-
-                for (String attrName : conversionData.keySet()) {
-                    Log.d(TAG, "attribute: " + attrName + " = " + conversionData.get(attrName));
-                }
-
-            }
-
-            @Override
-            public void onAttributionFailure(String errorMessage) {
-                Log.d(TAG, "error onAttributionFailure : " + errorMessage);
-            }
-        };
-        AppsFlyerLib.getInstance().init(AF_DEV_KEY, conversionListener, getApplicationContext());
-        AppsFlyerLib.getInstance().startTracking(getApplication());
 
         Button button = (Button) findViewById(R.id.btn_log);
         button.setOnClickListener(new View.OnClickListener() {
@@ -211,8 +231,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        ALYLogin.guestLogin("player001");
+        
+        ALYLogin.guestLogin("player0000001");
 //        ALYLogin.loginWithAASDK("facebook","player001","ggid1111111","logintoken",null);
+        Log.d(TAG, "login: ");
 
     }
 
